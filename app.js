7381c421982c2f5646daceec75c145f5fbe280bb
Solomon Abraha <solomon@Solomons-MacBook-Pro.local>
2020-07-17 15:10:34 -0600

    [variables] Add 5 variables

---
+++ app.js
@@ -0,0 +1,25 @@
+// 5 variables of different types
+//string
+//number
+//boolean
+//object (4 different key/value pairs) [string],[number], [array], [boolean]
+
+
+let saying = 'whats up';
+console.log(saying);
+
+let x = 25
+console.log(x)
+
+const cars = ['honda', 'ford', 'audi'];
+console.log(cars);
+
+let random = false;
+console.log(random);
+
+const broncos = {
+    location: 'Denver',
+    mascot: 'Bronco',
+    superbowls: 'three',
+}
+console.log(broncos);
